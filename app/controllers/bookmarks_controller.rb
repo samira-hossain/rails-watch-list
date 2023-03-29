a128a0cb8f760000d565313552728cf6c6e89507
samira hossain <samira.hossain@outlook.com>
2023-03-30 00:41:07 +1000

    Watch list updated

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,30 @@
 class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmark_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+  # As a user, I can see all my movie lists
+  # As a user, I can create a movie list
+  # As a user, I can see the details of a movie list
+  # As a user, I can bookmark a movie inside a movie list
+  # As a user, I can destroy a bookmark
+
   # root "articles#index"
+  resources :lists, only: [:index, :show, :new, :create, :destroy ]
+  resources :bookmarks
 end
